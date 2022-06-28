efc2e2bbe14b959374f1b42ce9edadb1b9de98b2
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2022-06-29 01:51:35 +0530

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,12 @@ logger = logging.getLogger(__name__)
 
 
 class HiMod(loader.Module):
-    string = {"name": "HiMod", "help": ".hi"}
+    string = {"name": "HiMod", "description": "Hi",
+              "usage": ".hi", "developer": "Saber"}
+
+    def __init__(self):
+        self.config = loader.ModuleConfig(loader.ConfigValue(
+            "Hi", "Hi", "Hi"), validator=loader.validators.String(1))
 
     async def client_ready(self, client, db):
         self.client = client
